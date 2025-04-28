f0cc933d5cb4be73851acfb2474572d945b49001
Guillaume Pollastri <gpollast@bess-f2r2s14.clusters.42paris.fr>
2025-04-28 14:27:22 +0200

    ft_strrchr.c

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/26 23:04:18 by gpollast          #+#    #+#             */
-/*   Updated: 2025/04/28 14:26:57 by gpollast         ###   ########.fr       */
+/*   Created: 2025/04/28 14:16:49 by gpollast          #+#    #+#             */
+/*   Updated: 2025/04/28 14:25:41 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <libft.h>
 
-#include <stddef.h>
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-
-#endif
+	i = 0;
+	while (s[i])
+		i++;
+	while (i > 0)
+	{
+		if (s[i] == (char) c)
+			return ((char *) s + i);
+		i--;
+	}
+	return (NULL);
+}
