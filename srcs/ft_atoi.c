61ee036995c9473783245f649244f87bfa293c63
Guillaume <guillox01@gmail.com>
2025-04-29 00:34:28 +0200

    atoi corrige

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,11 +6,18 @@
 /*   By: gpollast <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:32:16 by gpollast          #+#    #+#             */
-/*   Updated: 2025/04/28 11:57:56 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/04/29 00:32:01 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
+
+static int 	is_space(const char c)
+{
+	if ((c >= 7 && c <= 13) || c == ' ')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -21,14 +28,14 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	res = 0;
 	sign = 1;
+	while (is_space(nptr[i]))
+		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	if (!(nptr[i] >= '0' && nptr[i] <= '9'))
-		return (0);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		res = (res * 10) + (nptr[i] - '0');
@@ -36,3 +43,14 @@ int	ft_atoi(const char *nptr)
 	}
 	return (sign * res);
 }
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (0);
+	printf("%d\n", ft_atoi(av[1]));
+	printf("%d\n", atoi(av[1]));
+}*/
