34762f30009774300c4825127193b07c831b0393
Guillaume Pollastri <gpollast@bess-f2r2s14.clusters.42paris.fr>
2025-04-28 15:30:30 +0200

    ft_memchr, memcmp

---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 15:15:14 by gpollast          #+#    #+#             */
+/*   Updated: 2025/04/28 15:29:34 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*ptr1;
+	char	*ptr2;
+
+	ptr1 = (char *) s1;
+	ptr2 = (char *) s2;
+	if (n == 0)
+		return (0);
+	while (n > 0)
+	{
+		if (*ptr1 != *ptr2)
+			return ((int)(*ptr1 - *ptr2));
+		ptr1++;
+		ptr2++;
+		n--;
+	}
+	return ((int)(*ptr1 - *ptr2));
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	printf("%d\n", memcmp("toto", "tata", 4));
+	printf("%d\n", memcmp("toto", "toto", 4));
+	printf("%d\n", memcmp("toto", "tutu", 4));
+}*/
