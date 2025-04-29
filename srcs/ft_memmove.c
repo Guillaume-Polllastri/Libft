90998b377799213559d4cdaa690576a1c8ce7614
Guillaume Pollastri <gpollast@bess-f3r4s7.clusters.42paris.fr>
2025-04-29 12:22:51 +0200

    chgt memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 23:20:05 by gpollast          #+#    #+#             */
-/*   Updated: 2025/04/28 21:48:06 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:29:49 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,30 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	const char	*ptr_src;
 	char		*ptr_dest;
+	size_t		len_src;
+	size_t		len_dest;
 	size_t		i;
 
 	ptr_src = (const char *) src;
 	ptr_dest = (char *) dest;
+	len_src = ft_strlen(ptr_src);
+	len_dest = ft_strlen(ptr_dest);
 	i = 0;
-	while (i < n)
+	if (ptr_dest < ptr_src)
 	{
-		ptr_dest[i] = ptr_src[i];
-		i++;
+		while (i < n)
+		{
+			ptr_dest[i] = ptr_src[len_src];
+			i++;
+			len_src--;
+		}
 	}
+	i = 0;
+	else
+		while (i < n)
+		{
+			ptr_dest[i] = ptr_src[i];
+			i++;
+		}
 	return (dest);
 }
