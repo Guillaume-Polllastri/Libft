ac1f4ae43a584b3f9c6c0780235fabf7b846b890
Guillaume Pollastri <gpollast@bess-f3r4s7.clusters.42paris.fr>
2025-04-29 12:24:12 +0200

    chgt strlcat

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:50:39 by gpollast          #+#    #+#             */
-/*   Updated: 2025/04/28 20:19:43 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/04/29 12:23:30 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 {
 	size_t	i;
 	size_t	j;
-	size_t	len;
+	size_t	dst_len;
 
 	i = 0;
 	j = 0;
 	while (i < siz && dst[i])
 		i++;
-	len = i;
+	dst_len = i;
 	while ((i + 1) < siz && src[j])
 	{
 		dst[i] = src[j];
@@ -36,5 +36,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 		dst[i] = '\0';
 		i++;
 	}
-	return (len + j);
+	return (dst_len + j);
 }
