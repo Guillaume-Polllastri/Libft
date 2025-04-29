44e1fa95547f35d0076011164b85681baff4c621
Guillaume Pollastri <gpollast@bess-f3r4s7.clusters.42paris.fr>
2025-04-29 10:33:34 +0200

    atoi syntax

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpollast <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/25 18:32:16 by gpollast          #+#    #+#             */
-/*   Updated: 2025/04/29 07:59:53 by gpollast         ###   ########.fr       */
+/*   Created: 2025/04/29 10:32:34 by gpollast          #+#    #+#             */
+/*   Updated: 2025/04/29 10:33:09 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-static int 	is_space(const char c)
+static	int	is_space(const char c)
 {
 	if ((c >= 9 && c <= 13) || c == ' ')
 		return (1);
