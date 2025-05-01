88c838c6ed1dcf580241c933ad74ab551a7bd379
Guillaume Pollastri <gpollast@bess-f4r3s2.clusters.42paris.fr>
2025-05-01 14:30:29 +0200

    ft_substr

---
+++ srcs/ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 14:16:58 by gpollast          #+#    #+#             */
+/*   Updated: 2025/05/01 14:30:03 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ns;
+	int		i;
+
+	ns = (char *) malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (s[start])
+	{
+		ns[i] = s[start];
+		i++;
+		start++;
+	}
+	ns[i] = '\0';
+	return (ns);
+}
