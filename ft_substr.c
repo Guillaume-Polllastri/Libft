b5a453f8a0ef4a86238fce880194e8e3cdc03ac0
Guillaume Pollastri <gpollast@bess-f4r5s6.clusters.42paris.fr>
2025-05-07 11:06:45 +0200

    maj erreur fonctions

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 14:16:58 by gpollast          #+#    #+#             */
+/*   Updated: 2025/05/07 10:09:14 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ns;
+
+	if (len < (ft_strlen(s) - start))
+		len = len;
+	else
+		len = ft_strlen(s) - start;
+	if (start >= ft_strlen(s))
+		len = 0;
+	ns = (char *) ft_calloc(len + 1, sizeof(char));
+	if (!ns)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ns);
+	ft_strlcpy(ns, s + start, len + 1);
+	return (ns);
+}
